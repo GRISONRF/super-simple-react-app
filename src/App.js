71849cbbe73723f942168f38189e2b414dae2488
grisonrf <grisonrf@gmail.com>
2022-09-16 13:16:40 -0600

    App is ready!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const url = 'https://api.randomuser.me/'
+  const [firstName, setFirstName] = useState(null)
+  const [lastName, setLastName] = useState(null)
+  const [pic, setPic] = useState(null)
+
+  useEffect(() => {
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data['results'][0]['name']['first'])
+      setFirstName(data['results'][0]['name']['first'])
+      setLastName(data['results'][0]['name']['last'])
+      setPic(data['results'][0]['picture']['large'])
+    })
+  }, [])
+
+
+  if (url) {
+    return (
+      <div>
+        <h1>Every time you refresh the page you will see a different first and last name and picture.</h1>
+        <h2>{firstName} {lastName}</h2>
+        <img src={pic}></img>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
